6ea556c7fc56af9f48a40c1035e113d792066ff4
Gutianpei <brucegu@terpmail.umd.edu>
2020-03-21 16:46:08 -0400

    resnet50 and dataset v1 added

--- dataset.py
+++ dataset.py
@@ -1,1 +1,17 @@
 '''Construct dataset'''
+import os
+import torch
+import random
+import json
+import numpy as np
+
+class dataset():
+    def __init__(self, dataset_dir):
+        self.dataset_dir = dataset_dir
+
+    def get_dict(self,index):
+        js_data = json.load(self.dataset_dir)   #load json to dict
+        return js_data
+
+    def get_img_list(self):
+        
--- train.py
+++ train.py
@@ -1,1 +1,5 @@
 '''training'''
+import torchvision
+
+model = torchvision.models.resnet50(pretrained=True)
+model = model.cuda()
