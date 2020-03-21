eca78a1fd4fe2cb4f78bb0d63858b715a3b93435
Gutianpei <brucegu@terpmail.umd.edu>
2020-03-21 15:16:14 -0400

    structure pose_estimator built

--- utils/pose_estimator.py
+++ utils/pose_estimator.py
@@ -1,25 +1,42 @@
-import json
-
-
 ''' utils to generate pose estimators for head, upper body, lower body and feet.
     Alphapose output format at https://github.com/MVIG-SJTU/AlphaPose/blob/master/docs/output.md
 '''
 
-def extract_kp(img_name, json_path):
-    '''Extract ALL keypoints from json'''
+def json_to_dict(json_path):
+    '''Convert json file to dictionary '''
+
+    return data
+
+def extract_kps(img_name, json_path):
+    ''' Extract ALL keypoints from json
+        Args:
+            img_name: image name to visualize
+            json_path: path to Alphapose output
+        Return:
+            kps: keypoints as a list
+        '''
+    return kps
+
+def get_kps_idx(img_name, json_path, indices):
+    ''' Extract keypoints with index in indices list '''
 
     return kps
 
-def vis_kps(kp_idx, img_name, json_path):
-    '''Visualize keypoints
+def get_head():
+    ''' Get head estimator and block the rest image with black color '''
 
-    Args:
-        kps_idx (int list): keypoints indice in Alphapose output json file
-        img_name: image name to visualize
-        json_path: path to Alphapose output
-    Return:
-        img_vis: image with keypoints visualization
-    '''
-    kps = extract_kp(img_name, json_path)
-    kp = 
-    return img_vis
+
+def get_upper():
+
+
+def get_lower():
+
+
+def get_feet():
+
+
+def get_estimator():
+    ''' Get four estimators for image_name '''
+
+def get_all_estimator():
+    ''' Get four estimators for all images '''
