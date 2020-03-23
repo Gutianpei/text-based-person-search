7760132e5832ae499ee2acbe178a06c57fad78cb
Gutianpei <brucegu@terpmail.umd.edu>
2020-03-23 17:30:49 -0400

    v1 model built

--- dataset.py
+++ dataset.py
@@ -1,34 +1,43 @@
 '''Construct dataset'''
 import os
-import torch
+from keras.preprocessing import image as img_util
+from keras.applications.resnet50 import preprocess_input
 import random
 import json
 import numpy as np
-import cv2
 
 class dataset():
-    def __init__(self, dataset_dir, new_height, new_width):
+    def __init__(self, dataset_dir, new_height, new_width, ratio):
         self.dataset_dir = dataset_dir
         self.new_height = new_height
-		self.new_width = new_width
-
+        self.new_width = new_width
+        self.ratio = ratio
+        
     def get_img_caption(self):
         js_data = json.load(self.dataset_dir + "/captions_all.json")
         # Check captions.json for format
+        data_len = len(js_data)
+        ratio_iter = self.ratio*data_len
         images = []
         captions = []
-        for item in js_data:
-            #Read image
-            image = cv2.imread("/imgs" + item["file_path"])
-            image = cv2.resize(image, (self.new_width, self.new_height))
-            image = image[:,:,::-1]
-            images.append(image.numpy())
+        for idx, item in enumerate(js_data):
+            if idx < data_len:
+                img_path = self.dataset_dir + "/imgs/" + item["file_path"]
+                img = image.load_img(img_path, target_size=(self.new_width, self.new_height))
+                x = image.img_to_array(img)
+                x = np.expand_dims(x, axis=0)
+                images.append(x)
+                #Read image with pytorch
+    #            image = cv2.imread(self.dataset_dir + "/imgs/" + item["file_path"])
+    #            image = cv2.resize(image, (self.new_width, self.new_height))
+    #            image = image[:,:,::-1]
+    #            images.append(image.numpy())
 
-            #Read captions
-            #TODO!
+                #Read captions
+                #TODO!
 
 
-        images = np.array(images, np.float32)
-        images = torch.from_numpy(images).float()
+#        images = np.array(images, np.float32)
+        images = preprocess_input(images)
 
         return images, captions
--- test.py
+++ test.py
@@ -1,2 +1,21 @@
 '''Script for testing
 '''
+from keras.models import model_from_yaml
+import numpy
+import os
+import dataset
+
+def main():
+	yaml_file = open('model.yaml', 'r')
+	loaded_model_yaml = yaml_file.read()
+	yaml_file.close()
+	loaded_model = model_from_yaml(loaded_model_yaml)
+	# load weights into new model
+	loaded_model.load_weights("model.h5")
+	print("Loaded model from disk")
+	 
+	# evaluate loaded model on test data
+	loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+	score = loaded_model.evaluate(X, Y, verbose=0)
+	print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
+	
