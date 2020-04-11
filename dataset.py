92918f6bbab631b29c3b4b1ab03433d1211992c0
Gutianpei <brucegu@terpmail.umd.edu>
2020-04-11 15:03:08 -0400

    resnet ready

--- dataset.py
+++ dataset.py
@@ -1,43 +1,47 @@
 '''Construct dataset'''
 import os
-from keras.preprocessing import image as img_util
-from keras.applications.resnet50 import preprocess_input
+import cv2
 import random
 import json
 import numpy as np
+from gensim.models import Word2Vec
+from keras.preprocessing import sequence
+from keras.applications.resnet50 import preprocess_input
 
 class dataset():
-    def __init__(self, dataset_dir, new_height, new_width, ratio):
+    def __init__(self, dataset_dir, new_height, new_width):
         self.dataset_dir = dataset_dir
         self.new_height = new_height
         self.new_width = new_width
-        self.ratio = ratio
-        
-    def get_img_caption(self):
-        js_data = json.load(self.dataset_dir + "/captions_all.json")
+
+    def get_img(self):
+        js_data = json.load(open(self.dataset_dir + "/caption_all.json"))
         # Check captions.json for format
-        data_len = len(js_data)
-        ratio_iter = self.ratio*data_len
         images = []
         captions = []
-        for idx, item in enumerate(js_data):
-            if idx < data_len:
-                img_path = self.dataset_dir + "/imgs/" + item["file_path"]
-                img = image.load_img(img_path, target_size=(self.new_width, self.new_height))
-                x = image.img_to_array(img)
-                x = np.expand_dims(x, axis=0)
-                images.append(x)
-                #Read image with pytorch
-    #            image = cv2.imread(self.dataset_dir + "/imgs/" + item["file_path"])
-    #            image = cv2.resize(image, (self.new_width, self.new_height))
-    #            image = image[:,:,::-1]
-    #            images.append(image.numpy())
+        for item in js_data:
+            #Read image
+            #print(os.getcwd())
+            image = cv2.imread(self.dataset_dir + "/imgs/" + item["file_path"])
+            image = cv2.resize(image, (self.new_width, self.new_height))
+            #print(image)
+            image = image[:,:,::-1] #BGR to RGB
+            images.append(image)
+            print(len(images))
+            #Read captions
+            #TODO!
 
-                #Read captions
-                #TODO!
+        #return images, captions
+        return np.array(images)
 
-
-#        images = np.array(images, np.float32)
-        images = preprocess_input(images)
-
-        return images, captions
+    # def get_caption(self):
+    #     # word embedding training (could change to pre-trained one)
+    #     word_model = Word2Vec(captions, size=300, min_count=1)
+    #     word_model.wv.save_word2vec_format('word_model.bin')
+    #
+    #     # convert
+    #     captions_embeddings = np.array([word_model[i] for i in captions])
+    #     captions_embeddings = sequence.pad_sequences(captions_embeddings, dtype='float32', padding='post')
+    #
+    #
+    #     return captions
--- train.py
+++ train.py
@@ -1,105 +1,46 @@
 '''training'''
 
-from gensim.models import Word2Vec
+#import torchvision
 import numpy as np
-import tensorflow as tf
-from keras.preprocessing import sequence
 import dataset
-from scipy import spatial
-from resnet50 import run_resnet50
-import keras.backend as K
-from keras.models import model_from_yaml
-import os
-from keras.preprocessing import sequence
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, InputLayer
+from keras.models import Sequential, Model
+from keras import optimizers
+from keras.applications.resnet50 import ResNet50
+import keras
+# load images and captions
+dpath = "../datasets/CUHK-PEDES"
+IMG_HEIGHT = 256
+IMG_WIDTH = 128
+# https://towardsdatascience.com/deep-learning-using-transfer-learning-python-code-for-resnet50-8acdfb3a2d38
+data = dataset.dataset(dataset_dir = dpath, new_width = IMG_WIDTH, new_height = IMG_HEIGHT)
+images= data.get_img()
 
-def euclidean_distance_loss(y_true, y_pred):
-	''' l2 norm loss function
-	
-		Args: 
-			y_pred: bi-lstm output vector
-			y_true: resnet50 output vector
-		Return:
-			keras backend loss function
-	'''
-	return K.sqrt(K.sum(K.square(y_pred - y_true), axis=-1))
+print(images.shape)
+input_shape = images.shape[1:]
+###### Renset50 ############
+restnet = ResNet50(include_top=False, weights='imagenet', input_shape=(IMG_HEIGHT,IMG_WIDTH,3))
+output = restnet.layers[-1].output
+output = keras.layers.Flatten()(output)
+restnet = Model(restnet.input, output=output)
+for layer in restnet.layers:
+    layer.trainable = False
+restnet.summary()
 
-def cos_distance_loss(y_true, y_pred):
-	''' cos distance loss function'''
-	y_true = K.l2_normalize(y_true, axis=-1)
-	y_pred = K.l2_normalize(y_pred, axis=-1)
-	return K.mean(1 - K.sum((y_true * y_pred), axis=-1))
-	
-	
-def main():
-	# Hyper params and configs #
-	use_cuda = False 
-	lr = 0.00015
-	batch_size = 128
-	model_path = "models"
-	dpath = "PATH TO DATASET" #dataset path
-	ratio = 0.6	 #train/test split ratio
-	max_features = 20000
-	maxlen = 100
-	activition = "relu"
-	############################
-	#setup gpu
-	if use_cuda = True:
-		config = tf.ConfigProto( device_count = {'GPU': 1 , 'CPU': 4} ) 
-		sess = tf.Session(config=config) 
-		keras.backend.set_session(sess)
-	
-	curr_path = os.getcwd()
-	# Load images and captions #
-	data = dataset.dataset(dataset_dir = dpath, new_width = 128, new_height = 256, ratio = 0.6)
-	images, captions = data.get_img_caption()
-	############################
 
-	###### Renset50 ############ 
-	resnet_feat = run_resnet50(imgs = images)
-	print("Feature extraced by resnet50, feature vector shape: " + str(resnet_feat.shape))
-	print("#"*28 + "\n")
-	############################
+##### Model #############
+model = Sequential()
+model.add(restnet)
+model.add(Dense(512, activation='relu', input_shape=input_shape))
+model.add(Dropout(0.3))
 
-	###### Bi-LSTM #############
-	print("Building Bi-LSTM")
-	#tip: dataset.py里面提取caption还没写，我不知道要把caption变成什么格式，下面这些能不能加到dataset.py里？因为test的时候还要再用一次
 
-	# word embedding training (could change to pre-trained one)
-	word_model = Word2Vec(captions, size=300, min_count=1)
-	word_model.wv.save_word2vec_format('word_model.bin')
-
-	# convert
-	captions_embeddings = np.array([word_model[i] for i in captions])
-	captions_embeddings = sequence.pad_sequences(captions_embeddings, dtype='float32', padding='post')
-	
-	model = Sequential()
-	model.add(Embedding(max_features, 128, input_length=maxlen))
-	model.add(Bidirectional(LSTM(64)))
-	model.add(Dropout(0.5))
-	model.add(Dense(1, activation=activition))
-	
-	model.compile("adam",loss=cos_distance_loss,metrics=['accuracy']) #or loss=euclidean_distance_loss 
-
-#	model.fit(x_train, resnet_feat,
-#						batch_size=batch_size,
-#						epochs=100,
-#						validation_data=[x_test, y_test])
-	############################
-
-	###### save model ##########
-	mpath = os.join(curr_path,model_path)
-	print("Saving model to " + mpath)
-	
-	model_yaml = model.to_yaml()
-	with open(mpath + "model.yaml", "w") as yaml_file:
-		yaml_file.write(model_yaml)
-	# serialize weights to HDF5
-	model.save_weights("model.h5")
-	print("Saved model to " + mpath + "model.yaml")
-	
-	
-if __name__ == "__main__":
-	main()
-
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=2e-5),
+              metrics=['accuracy'])
+# history = model.fit_generator(train_generator,
+#                               steps_per_epoch=100,
+#                               epochs=100,
+#                               validation_data=val_generator,
+#                               validation_steps=50,
+#                               verbose=1)
